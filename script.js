5558eac8f196cef3ffaae06189425050acf5fefe
kevinrizki1019 <13518100@std.stei.itb.ac.id>
2020-08-30 18:54:06 +0700

    make responsive for dekstop view (2)

--- script.js
+++ script.js
@@ -11,5 +11,5 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 const button = document.querySelector(".button");
 button.addEventListener('click', function() {
-    alert("Hello " + document.getElementById("name-form").value + ", I already receive your message but I'm not ready to processing anything for you :(, please comeback later.\n\nYour email address: " + document.getElementById("email-form").value + "\nYour message: " + document.getElementById("message-form").value);
+    alert("Hello " + document.getElementById("name-form").value + ", I already can receive your message but I'm not ready to processing anything even save your information for you :(, please comeback later.\n\nYour email address: " + document.getElementById("email-form").value + "\nYour message: " + document.getElementById("message-form").value);
 })
