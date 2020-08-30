1cfc3b045995f778bf40ea5b9d8b3d514c8bfeef
kevinrizki1019 <13518100@std.stei.itb.ac.id>
2020-08-30 19:01:10 +0700

    Refactory code (maybe finish)

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Profile-Page---Web-Based-Development
 Simple HTML, CSS, (and JavaScript) web page that show information about personal profile.
+
+Things I implemented in this project:
+* Smooth scrolling in the same HTML page using DOM. 
+* CSS Flexox and Grid.
+* Responsive web using media queries.
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Query selector for smooth scrolling
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -8,7 +9,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 })
 
-
+// Respons for HTML form
 const button = document.querySelector(".button");
 button.addEventListener('click', function() {
     alert("Hello " + document.getElementById("name-form").value + ", I already can receive your message but I'm not ready to processing anything even save your information for you :(, please comeback later.\n\nYour email address: " + document.getElementById("email-form").value + "\nYour message: " + document.getElementById("message-form").value);
